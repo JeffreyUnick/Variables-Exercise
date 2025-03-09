0b8e320ae2c1f4f9e16f438c591993ef4fb21372
UNICK J <jeffreyodeloui@gmail.com>
2025-03-09 15:27:58 -0400

    Exercise done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string middleName = "Johnny";
+            int weight = 203;
+            char gender = 'm';
+            bool likeBaseball = false;
+            double averageTime = 35.6;
+            decimal averageScore = 12.78m;
+        
+            Console.WriteLine($"Donald J BECKER is a baseball player.\n His middle name is: {middleName}\n " +
+                              $"His gender is : {gender}\n Saying that he doesn't like baseball is obviously {likeBaseball}." +
+                              $"\n He weight {weight} lbs.\n And his score is {averageScore} on average time {averageTime} seconds. ");
         }
     }
 }
